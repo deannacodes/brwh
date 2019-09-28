9116c2b1ad74f38b7b2b2e7a0bddd13ba02c8833
dstreff <deanna@sandstonestudio.com>
2019-09-28 23:17:09 +0900

    dashboard updates

--- src/components/UserDataView.vue
+++ src/components/UserDataView.vue
@@ -12,7 +12,7 @@
             start-color="#4eaba8"
             stop-color="#489290"
             inner-stroke-color="#efefef"
-            stroke-width=20
+            stroke-width="20"
           >
             <h1>{{viewingUser.overall_score}}</h1>
           </radial-progress-bar>
@@ -33,7 +33,7 @@
                 start-color="#4eaba8"
                 stop-color="#489290"
                 inner-stroke-color="#efefef"
-                stroke-width=5
+                stroke-width="5"
               >
                 <h6>{{score}}</h6>
               </radial-progress-bar>
@@ -43,33 +43,46 @@
       </div>
     </div>
 
+    <div class="score-card score-card-main">
+      <h3>Java Repository Stats</h3>
+      <hr />
+      <table class="table table-borderless" style="border-top:none; text-align: center">
+        <thead style="border-top:none">
+          <tr style="border-top:none">
+            <th scope="col" class="br" style="border-top:none">Number of Repos:</th>
+            <th scope="col" class="br" style="border-top:none">Average Stars per Repo:</th>
+            <th scope="col" class="br" style="border-top:none">Average Closed Issues per Repo:</th>
+            <th scope="col" style="border-top:none">Average Fork Count:</th>
+          </tr>
+        </thead>
+        <tbody style="border-top:none">
+          <tr>
+            <td class="br">{{viewingUser.num_of_java_repos}}</td>
+            <td class="br">{{viewingUser.avg_stars_count_per_repo}}</td>
+            <td class="br">{{viewingUser.closed_issue_ratio.toFixed(2)}}</td>
+            <td>{{viewingUser.avg_fork_count}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     <div class="row justify-content-center">
       <div class="score-card">
-        <h3>Java Repository Stats</h3>
+        <h3>Productivity</h3>
         <hr />
         <div class="java-stat row">
-          <div class="col-md-9">Number of Repos:</div>
-          <div class="col-md-3">{{viewingUser.num_of_java_repos}}</div>
-        </div>
-        <div class="java-stat row">
-          <div class="col-md-9">Average Stars per Repo:</div>
-          <div class="col-md-3">{{viewingUser.avg_stars_count_per_repo}}</div>
-        </div>
-        <div class="java-stat row">
-          <div class="col-md-9">Average Closed Issues per Repo:</div>
-          <div class="col-md-3">{{viewingUser.closed_issue_ratio.toFixed(2)}}</div>
-        </div>
-        <div class="java-stat row">
-          <div class="col-md-9">Average Fork Count:</div>
-          <div class="col-md-3">{{viewingUser.avg_fork_count}}</div>
-        </div>
-        <div class="java-stat row">
           <div class="col-md-9">Code Additions:</div>
-          <div class="col-md-3">{{viewingUser.code_additions}}</div>
+          <div
+            class="col-md-3"
+            style="color: #86c959;font-weight:bold;text-align:right;"
+          >+ {{viewingUser.code_additions}}</div>
         </div>
         <div class="java-stat row">
-          <div class="col-md-9">Average Fork Count:</div>
-          <div class="col-md-3">{{viewingUser.code_deletions}}</div>
+          <div class="col-md-9">Code Deletions:</div>
+          <div
+            class="col-md-3"
+            style="color: #b0291a; font-weight:bold;text-align:right;"
+          >- {{viewingUser.code_deletions}}</div>
         </div>
       </div>
 
@@ -91,55 +104,80 @@
       </div>
     </div>
     <div class="row justify-content-center">
-      <div class="score-card">
-        <h3>Versatility</h3>
+      <div class="score-card score-card-third">
+        <h3>Code Quality Index</h3>
         <hr />
-        <div class="row" v-for="dependency in viewingUser.top_dependencies" v-bind:key="type">
-          <div class="col-md-9 score-name">{{ dependency[0] }}</div>
-          <div class="col-md-3">{{ dependency[1] }}</div>
+        <div class="align-self-center justify-self-center">
+          <div class="row">
+            <radial-progress-bar
+              :diameter="200"
+              :completed-steps="viewingUser.community_scores.code_quality_index"
+              :total-steps="100"
+              start-color="#4eaba8"
+              stop-color="#489290"
+              inner-stroke-color="#efefef"
+              stroke-width="5"
+              style="margin: 0 auto;"
+            >
+              <h6>{{viewingUser.community_scores.code_quality_index}}</h6>
+            </radial-progress-bar>
+          </div>
         </div>
       </div>
 
-      <div class="score-card">
+      <div class="score-card score-card-two-thirds">
         <h3>Community Engagement</h3>
         <hr />
         <div class="align-self-center justify-self-center">
           <div class="row">
-            <div class="col-md-9 score-name">
-              <p>Comments Quality Score</p>
+            <div class="col-md-6 score-name text-center">
+              <h6>Comments Quality Score</h6>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6 score-name  text-center" >
+              <h6>PR Quality Score</h6>
+            </div>
+            
+          </div>
+          <div class="row">
+            <div class="col-md-6">
               <radial-progress-bar
-                :diameter="65"
+                :diameter="150"
                 :completed-steps="viewingUser.community_scores.comments_quality_score"
                 :total-steps="100"
                 start-color="#4eaba8"
                 stop-color="#489290"
                 inner-stroke-color="#efefef"
-                stroke-width=5
+                stroke-width="5"
+                style="margin: 0 auto;"
               >
                 <h6>{{viewingUser.community_scores.comments_quality_score}}</h6>
               </radial-progress-bar>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-9 score-name">
-              <p>PR Quality Score</p>
-            </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
               <radial-progress-bar
-                :diameter="65"
+                :diameter="150"
                 :completed-steps="viewingUser.community_scores.pr_quality_score"
                 :total-steps="100"
                 start-color="#4eaba8"
                 stop-color="#489290"
                 inner-stroke-color="#efefef"
-                stroke-width="5"
+                stroke-width="5"                
+                style="margin: 0 auto;"
               >
                 <h6>{{viewingUser.community_scores.pr_quality_score}}</h6>
               </radial-progress-bar>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="row align-items-center justify-content-center">
+      <div class="score-card">
+        <h3>Versatility</h3>
+        <hr />
+        <div class="row" v-for="dependency in viewingUser.top_dependencies" v-bind:key="type">
+          <div class="col-md-10 score-name">{{ dependency[0] }}</div>
+          <div class="col-md-2">{{ dependency[1] }}</div>
         </div>
       </div>
     </div>
